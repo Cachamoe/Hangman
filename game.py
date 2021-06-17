df47ae47e5ae9320287ba0b801f23c7d23a482ee
Caleb Chaney <calebc347@gmail.com>
2021-06-17 11:03:25 -0500

    Added and Updated game.py

---
+++ game.py
@@ -0,0 +1,34 @@
+import random, string
+
+def hangman(words_list):
+
+
+
+
+
+
+
+
+
+def restart = 
+	restart = input("Do you wnat to play again(y/n): ").lower()
+	if restart == "y":
+		return hangman(words)
+	else:
+		return "Thanks for playing"
+
+
+words = ['yellow', 'orange', 'blue', 'red', 'green', 'purple', 'brown', 'black', 'white', 'circle', 'rectangle', 'square', 'triangle', 'octagon', 'hexagon', 'pentagon',
+'oval', 'dog', 'cat', 'bird', 'elephant', 'lion', 'giraffe', 'zebra', 'monkey', 'flamingo', 'gorilla', 'fish', 'deer', 'elk', 'moose', 'buffalo', 'bison', 'turtle', 'alligator',
+'crocodile', 'meercat', 'penguin', 'antelope', 'seagull', 'cardinal', 'robin', 'beaver', 'pigs', 'chicken', 'rooster', 'pigeon', 'cow', 'rhinoceros', 'hippopotamus', 'ostrich',
+'hen', 'rat', 'mouse', 'fox', 'triceratops', 'velociraptor', 'peacock', 'wombat', 'wolverine', 'otter', 'aardvark', 'racoon', 'pufferfish', 'blender', 'refrigerator', 'chair', 
+'bed', 'table', 'counter', 'recliner', 'sofa', 'couch', 'kitchen', 'bedroom', 'pantry', 'foyer', 'bathroom', 'pool', 'den', 'car', 'plane', 'elevator', 'stairs', 'train', 
+'helicoptor', 'towel', 'rug', 'doormat', 'blanket', 'pillow', 'tree', 'log', 'oak', 'cedar', 'spruce', 'pine', 'redwood', 'flower', 'daisy', 'rose', 'dandelion', 'bush', 'shrub', 
+'house', 'apartment', 'condo', 'mansion', 'lodge', 'cabin', 'grass', 'pollen', 'bees', 'wasp', 'hornet', 'spaghetti', 'ravioli', 'fettuccine', 'tacos', 'burrito', 'chimichanga', 
+'quesadilla', 'tortilla', 'nachos', 'chips', 'queso', 'salsa', 'sandwich', 'burger', 'fries', 'soda', 'milk', 'water', 'pickles', 'ketchup', 'mustard', 'mayonnaise', 'relish', 
+'salt', 'vinegar', 'pepper', 'onion', 'tomato', 'lettuce', 'cheese', 'ginger', 'peppermint', 'lavender', 'cup', 'mug', 'glass', 'bottle', 'plate', 'napkin', 'spoon', 'fork', 
+'knife', 'spork', 'vacuum', 'light', 'lamp', 'fan', 'painting', 'artwork', 'frame', 'clock', 'time', 'mulch', 'pavement', 'sidewalk', 'street', 'streetlight', 'key', 'lock',
+'basket', 'tub', 'box', 'attic', 'shirt', 'shorts', 'shades', 'glasses', 'contacts', 'jeans', 'shoes', 'watch', 'belt', 'hat', 'fedora', 'fishing', 'hunt', 'hunting', 'swim', 
+'swimming', 'ski', 'skiing', 'snowboard', 'snowboarding', 'drive', 'driving', 'eat', 'eating', 'walk', 'walking', 'run', 'running', 'sprint', 'sprinting', 'open', 'opening',
+]
+hangman(words)
